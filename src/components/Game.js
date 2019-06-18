75727c06de8dc6e0292a09c268c6aa4ecc976e4e
Jansen <v.jansen.martin@gmail.com>
2019-06-18 16:04:56 -0700

    Pass recentSubmission as prop to RecentSubmission.js; Render recent submission on page

--- src/components/Game.js
+++ src/components/Game.js
@@ -59,7 +59,7 @@ class Game extends Component {
           { exampleFormat }
         </p>
 
-        <RecentSubmission />
+        <RecentSubmission recentSubmission={this.state.recentSubmission}/>
 
         <PlayerSubmissionForm receivePlayerSubmissionCallback={this.receivePlayerSubmission}/>
 
--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+  
+  const { recentSubmission } = props;
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{ recentSubmission }</p>
     </div>
   );
+}
+
+RecentSubmission.propTypes = {
+  recentSubmission: PropTypes.string,
 }
 
 export default RecentSubmission;
